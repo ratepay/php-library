4fc283f88a07eb0d1441d4953a5ab9276e42ed9e
schmittrigger <aarne.welschlau@ratepay.com>
2017-03-28 18:34:07 +0200

    update of version and readme from 0.9.1.1 to 0.9.1.2

--- src/Model/Request/SubModel/Constants.php
+++ src/Model/Request/SubModel/Constants.php
@@ -22,7 +22,7 @@ class Constants {
     /**
      * API SDK version
      */
-    const API_SDK_VERSION = "0.9.1.1";
+    const API_SDK_VERSION = "0.9.1.2";
 
     /**
      * Supported RatePAY API version
