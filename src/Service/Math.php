4277470800875851d3988add36f68d3b3f31341a
Aaron Whittington <aaron.whittington@ratepay.com>
2018-08-07 13:47:23 +0200

    bug/ETS-20_net-prices-setting-breaks-ratepay  add rounding

---
+++ src/Service/Math.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RatePAY\Service;
+
+
+/**
+ * Class Math static functions that do reusable calculations.
+ * @package RatePAY\Service
+ */
+class Math
+{
+
+    /**
+     * @param float $netPrice
+     * @param float|int $taxPercentage
+     * @return float
+     */
+    public static function netToGross($netPrice, $taxPercentage)
+    {
+        $withTax = $netPrice + $netPrice * $taxPercentage / 100;
+
+        return $withTax;
+    }
+
+}
